7c5db060fd67e458b8cdc87472ff2fe1c9c222ac
Nikolai Khriapov <kolyakhryapov@gmail.com>
2023-06-25 11:46:38 +0800

    1. Made the review word card flippable.

--- frontend/src/components/review/ReviewWordCard.jsx
+++ frontend/src/components/review/ReviewWordCard.jsx
@@ -1,8 +1,15 @@
+import {useState} from 'react';
 import {
     Heading, Box, Center, Flex, Stack
 } from '@chakra-ui/react';
 
-export default function ReviewWordCard({ reviewWord }) {
+export default function ReviewWordCard({reviewWord}) {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleFlip = () => {
+        setIsFlipped(!isFlipped);
+    };
+
     return (
         <Center py={6}>
             <Flex
@@ -19,17 +26,20 @@ export default function ReviewWordCard({ reviewWord }) {
                 boxShadow={'l'}
                 rounded={'md'}
                 overflow={'hidden'}
+                onClick={handleFlip}
+                style={{
+                    transform: isFlipped ? 'rotateY(180deg) scaleX(-1)' : 'rotateY(0deg)',
+                    transition: 'transform 0.5s',
+                }}
             >
-                <Box p={6}>
+                <Box p={6} style={{backfaceVisibility: 'hidden'}}>
                     <Stack spacing={2} align={'center'} mb={30}>
-                        <Heading
-                            fontSize={'8xl'} fontWeight={'thin'} fontFamily={'SimSun, body'}
-                        >
-                            {reviewWord.nameChineseSimplified}
+                        <Heading fontSize={isFlipped ? '4xl' : '8xl'} fontWeight={'thin'} fontFamily={'SimSun, body'}>
+                            {isFlipped ? reviewWord.pinyin : reviewWord.nameChineseSimplified}
                         </Heading>
-                    </Stack>
-                    <Stack>
-
+                        <Heading fontSize={isFlipped ? '4xl' : '8xl'} fontWeight={'thin'} fontFamily={'SimSun, body'}>
+                            {isFlipped ? reviewWord.nameEnglish : ''}
+                        </Heading>
                     </Stack>
                 </Box>
             </Flex>
