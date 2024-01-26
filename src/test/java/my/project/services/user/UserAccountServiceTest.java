18260f64c6cd14bcda077ce0e4f29a683c107e99
Nikolai Khriapov <kolyakhryapov@gmail.com>
2024-01-26 22:01:27 +0800

    Temporarily commented out some tests

--- src/test/java/my/project/services/user/UserAccountServiceTest.java
+++ src/test/java/my/project/services/user/UserAccountServiceTest.java
@@ -1,28 +1,13 @@
 package my.project.services.user;
 
 import my.project.config.AbstractUnitTest;
-import my.project.models.dto.user.UserDTO;
-import my.project.models.entity.user.RoleName;
-import my.project.models.entity.user.User;
-import my.project.models.mapper.user.RoleStatisticsMapper;
-import my.project.models.mapper.user.UserMapper;
 import my.project.repositories.user.UserRepository;
 import my.project.services.flashcards.ReviewService;
 import my.project.services.flashcards.WordService;
 import my.project.services.notification.NotificationService;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Spy;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import static my.project.util.data.TestDataUtil.generateUser;
-import static my.project.util.data.TestDataUtil.mockAuthentication;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willCallRealMethod;
 
 class UserAccountServiceTest extends AbstractUnitTest {
 
