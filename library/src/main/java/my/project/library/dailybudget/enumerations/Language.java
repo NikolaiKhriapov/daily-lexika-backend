baa2e2f60e164b58f5ef2ef0769e7b4fbdbf541d
Nikolai <kolyakhryapov@gmail.com>
2025-07-26 13:31:06 +0800

    Remove `daily-budget` project

--- library/src/main/java/my/project/library/dailybudget/enumerations/Language.java
+++
@@ -1,6 +0,0 @@
-package my.project.library.dailybudget.enumerations;
-
-public enum Language {
-    ENGLISH,
-    RUSSIAN
-}
--- library/src/main/java/my/project/library/dailybudget/enumerations/LogAction.java
+++
@@ -1,7 +0,0 @@
-package my.project.library.dailybudget.enumerations;
-
-public enum LogAction {
-    ACCOUNT_REGISTRATION,
-    ACCOUNT_DELETION,
-    EMAIL_UPDATE
-}
